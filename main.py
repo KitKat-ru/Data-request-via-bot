1ab39b70613e98e5a9d7da9b6c335baf00e2a336
KitKat-ru <althome195@gmail.com>
2022-07-09 23:30:05 +0300

    deploy_heroku8

--- main.py
+++ main.py
@@ -14,9 +14,15 @@ bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start'], func=lambda message: True)
 def start(message):
-    bot.send_message(f'Привет{message.from_user.first_name}, а дай номер.')
+    bot.send_message(message.chat.id, f'Привет{message.from_user.first_name}, а дай номер.')
+
+
+@bot.message_handler(commands=['start1'])
+def start(message):
+    bot.send_message(message.chat.id, f'Привет{message.from_user.first_name}, а дай номер.')
+
 
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
